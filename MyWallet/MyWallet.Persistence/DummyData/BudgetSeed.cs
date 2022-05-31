c5cba4a8ac0615a0cbde3278c76f7bbcb5ac923d
mateuszbabski <babskimateusz@gmail.com>
2022-05-31 16:24:15 +0200

    Update

--- MyWallet/MyWallet.Persistence/DummyData/BudgetSeed.cs
+++ MyWallet/MyWallet.Persistence/DummyData/BudgetSeed.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Persistence.DummyData
+namespace MyWallet.Persistence.DummyData
 {
-    internal class BudgetData
+    internal class BudgetSeed
     {
     }
 }
