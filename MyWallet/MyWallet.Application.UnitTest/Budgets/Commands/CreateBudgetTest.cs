208c59a03f53e427e3889edcf972fe03a6623ed3
mateuszbabski <babskimateusz@gmail.com>
2022-07-06 15:47:01 +0200

    Mock Repository for Unit Testing

---
+++ MyWallet/MyWallet.Application.UnitTest/Budgets/Commands/CreateBudgetTest.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Moq;
+using MyWallet.Application.Interfaces;
+using MyWallet.Application.Mapper;
+using MyWallet.Application.UnitTest.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWallet.Application.UnitTest.Budgets.Commands
+{
+    public class CreateBudgetTest
+    {
+
+        private readonly IMapper _mapper;
+        private readonly Mock<IBudgetRepository> _mockBudgetRepository;
+        public CreateBudgetTest()
+        {
+            _mockBudgetRepository = RepositoryMocks.GetBudgetRepository();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+        }
+    }
+}
---
+++ MyWallet/MyWallet.Application.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MyWallet.Application.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
